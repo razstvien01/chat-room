149f6d23981c57226fb8c7b09f8620c5508a3217
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-01 08:49:18 +0800

    added route for room

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -23,6 +23,7 @@ def generate_unique_code(length):
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+  session.clear()
   if request.method == "POST":
     name = request.form.get("name")
     code = request.form.get("code")
@@ -32,7 +33,7 @@ def home():
     if not name:
       return render_template("home.html", error="Please enter a name.", code=code, name=name)
     
-    if join is False and not code:
+    if join is not False and not code:
       return render_template("home.html", error="Please enter a room code.", code=code, name=name)
     
     room = code
@@ -42,7 +43,19 @@ def home():
     elif code not in rooms:
       return render_template("home.html", error="Room does not exists.", code=code, name=name)
       
+    session["room"] = room
+    session["name"] = name
+    return redirect(url_for("room"))
+      
   return render_template("home.html")
+
+@app.route("/room")
+def room():
+  room = session.get("room")
+  
+  if room is None or session.get("name") is None or room not in rooms:
+    return redirect(url_for("home"))
+  return render_template("room.html")
 
 if __name__ == "__main__":
   socketio.run(app, debug=True)

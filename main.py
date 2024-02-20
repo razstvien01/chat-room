e7a61867a97594f070c54ceb4f01a103ca596c78
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-20 12:28:39 +0800

    fixed bug about null room and implemented socket io disconnect

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def generate_unique_code(length):
     if code not in rooms:
       break
     
-    return code
+  return code
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -45,6 +45,8 @@ def home():
       
     session["room"] = room
     session["name"] = name
+    print(f"ROOM: {room}")
+    print(f"NAME: {name}")
     return redirect(url_for("room"))
 
   return render_template("home.html")
@@ -52,8 +54,8 @@ def home():
 @app.route("/room")
 def room():
   room = session.get("room")
-  # if room is None or session.get("name") is None or room not in rooms:
-  #   return redirect(url_for("home"))
+  if room is None or session.get("name") is None or room not in rooms:
+    return redirect(url_for("home"))
 
   return render_template("room.html")
 
@@ -73,10 +75,26 @@ def connect(auth):
   
   # * puts the user in the socket room
   join_room(room)
-  send({"name": name, "message": "has entered the roon"}, to=room)
+  send({"name": name, "message": "has entered the room"}, to=room)
   rooms[room]["members"] += 1
   print(f"{name} joined room {room}")
-  print("Hello world")
+  
+@socketio.on("disconnect")
+def disconnect():
+  room = session.get("room")
+  name = session.get("name")
+  
+  leave_room(room)
+  
+  if room in rooms:
+    rooms[room]["members"] -= 1
+    # ! delete room if no one's in the room
+    if rooms[room]["members"] <= 0:
+      del rooms[room]
+      
+  send({"name": name, "message": "has left the room"}, to=room)
+  print(f"{name} has left room {room}")
+  
 
 if __name__ == "__main__":
   socketio.run(app, debug=True)
